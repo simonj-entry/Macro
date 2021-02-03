cff1fc39d253c92dcf0cc28cecac66986bc3595f
simonj-entry <78244639+simonj-entry@users.noreply.github.com>
2021-02-03 14:30:42 +0900

    Delete Macro.js

--- Macro.js
+++
@@ -1,8 +0,0 @@
-console.log('simonj의 메크로 프로그램을 사용해주셔서 감사합니다.')
-function start() {
-$('.editDescriptionBtn').trigger('click')
-$('.editConfirmBtn').trigger('click')
-console.log('작품이 업데이트되었습니다')
-}
-start()
-setInterval(start, (30000))
